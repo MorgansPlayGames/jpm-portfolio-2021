f20a7ba4ac5b7f22c53480a431131bb62f56826d
Parker Morgan <jpmaster13@msn.com>
2021-05-01 00:55:37 -0600

    Added better stories, and updated the header

--- client/src/lib/historyList.js
+++ client/src/lib/historyList.js
@@ -4,7 +4,7 @@ const historyList = [
     subtitle: "All my school had World of Warcraft",
     age: "highschool",
     story:
-      "Way back in highschool, when I was permanantly grounded from videogames. So in retaliation, I took a ton of computer classes, and had World of Warcraft installed on the school's network so everyone could play it.",
+      "Way back in highschool, I was permanantly grounded from videogames. So in retaliation, I took a ton of computer classes, and had World of Warcraft installed on the school's network so everyone could play it. I created a private server that allowed my friends to play for free, hosting it on my family's computer, remoting in to start the server up when it shut down and no one was home. I also got to play halo by installing it on my iPod and plugging that into any computer. I probably caused a lot of network problems for my school.",
     tags: [
       { name: "All" },
       { name: "Highschool" },
--- client/src/lib/hobbyList.js
+++ client/src/lib/hobbyList.js
@@ -7,10 +7,18 @@ const hobbyList = [
         subtitle: "Dark Souls with guns",
         rating: "10/10",
         history:
-          "I played this game after beating dark souls 3. I still haven't beaten it. I have beaten Sekiro, Dark Souls 1, 2, and 3. This will hopefuly be my next win.",
-        timePlayed: "2021",
-        amountPlayed: "5 Days",
+          "I played Bloodborne just after beating Dark Souls 3. It is subversive with it's use of Victorian times, mixed with some H.P. Lovecraft madness. This is Dark Souls, but horror. Screaming pierces your ears from all sides, monsters jump out at you from behind, it is constantly dark, with only your torch to see. The first part of the game is a nighmare of difficulty, as it forces you to actually learn mechanics, rather than run through guns blazing. Trying to survive the high tensity atmosphere that this game bleeds, makes it all the better. Patience and careful timing are key to progressing through the game. I am currently trying to beat this one. I am making good progress!",
+        timePlayed: "2016, 2021",
+        amountPlayed: "a good month",
       },
+      {
+        name: "World of Warcraft",
+        subtitle: "Classic to BC",
+        rating: "10/10",
+        history: "The Warcraft series has been a big part of my life. I played Warcraft II, loved Starcraft, played tons of Warcraft III, and finally, after getting a good enough internet connection, and begging my dad for it, we purchased a subscription to World of Warcraft. I didn't actually get to play much, as I was grounded throughout Classic and most of The Burning Crusade, but I kept up on the lore, discussed it with my friends who actually played, and taught my sister how to play the game so I could watch her. I set up a bot so I could get to level 60, which got my account banned... I just wanted to kill Onyxia!",
+        timePlayed: "2006-2008",
+        amountPlayed:"Not a lot"
+      }
     ],
   },
   {
@@ -31,7 +39,7 @@ const hobbyList = [
       {
         name: "Horror DnD?!",
         subtitle: "Can you hear the call?",
-        experience: "Call of Cthulhu is great! I love it",
+        experience: "Call of Cthulhu is great! Horror and tabletop gaming are extremely fun, especially when you can get the mood right. When people feel creeped out, and are hesitant to proceed you know you have a fun game. Life is cheap in this game, guns can outright kill your character, you can go insane by looking at something you shouldn't. This fragility makes the horror all the more visceral. Modules are a lot easier to follow with investigation and clues being a pillar of the roleplay. The setting is usually the 1920's, making it easier for non-fantasy people to get in on it. I always suggest this game if people are sick of DnD.",
         time: "pre-pandemic",
         gameType: "Call of Cthulhu",
       },
