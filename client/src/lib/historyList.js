ad37c0639bb59714eb42085ff013dd2fde524ad4
Parker Morgan <jpmaster13@msn.com>
2021-05-01 18:28:22 -0600

    Added more story

--- client/src/lib/historyList.js
+++ client/src/lib/historyList.js
@@ -12,5 +12,15 @@ const historyList = [
       { name: "Funny" },
     ],
   },
+  {
+      title: "Grounded from Games",
+      subtitle: "I guess I'll make my own",
+      age: "Middle School",
+      story: "School was never my strong suit. I could not focus on or remember to do/turn in homework, I was disorganized, suffering from undiagnosed attention deficit disorder. The only thing my parents could think to do was to take away my videogames until I got my grades up. I unfortunately, even with the motivation of being able to play a game, I could not remember to do homework. Summers were particulary difficult as I didn't have many close friends or a social life. I did find a loophole though. If I could make my own game, I could play it. It is educating myself and parents agreed that it could be good for me. I started out reading a visual basic book, and make a two-dimention, top down airplane shooter. I didn't learn much, as I copied directly from the book, and spent a lot of time correcting my mistakes. The next project I created was a choose your own adventure game in C++ using the console in and console out. Watching the Lord of the Rings, the Two Towers features, I saw that they were using 3D modeling and simulation to create massive armies for the film. I got the free trial of Maya, and hated it, couldn't figure anything out. Then found Blender3D. I could follow the tutorials and created simple animations and models. I figured out how to render liquid simulations, and created a simple arena push opponent out game. All before highschool",
+      tags: [
+          {name:"All"},
+          {name:"Developer Origin Story"}
+      ]
+  }
 ];
 export default historyList;
